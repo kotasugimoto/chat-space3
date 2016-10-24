ed0d45f7effaed5b7b644c365138152a26edf18f
kotasugimoto <sugimotokouta@sugimoto-no-MacBook-Pro.local>
2016-10-24 14:24:31 +0900

    ルーティング、form_for

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,2 +1,20 @@
 class MessagesController < ApplicationController
+
+	def index
+		@message = Message.new
+	end
+
+	def new
+		@message = Message.new
+	end
+
+	def create
+		Message.create(message_params)
+		redirect_to controller: :messages, action: :index
+	end
+
+	private
+	def message_params
+		params.permit(:body)
+	end
 end
