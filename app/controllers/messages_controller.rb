ccc5b5f747966496f65185c14537ba52d6bd198c
kotasugimoto <sugimotokouta@sugimoto-no-MacBook-Pro.local>
2016-10-25 12:27:40 +0900

    修正

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,22 +1,21 @@
 class MessagesController < ApplicationController
+  def index
+	@message = Message.new
+  end
 
-	def index
-		@message = Message.new
-	end
+  def create
+	@message = Message.create(message_params)
+	if @message.save
+	  lash[:success] = "保存されました！"
+	  redirect_to action: :index
+    else
+	  flash[:error] = "空欄！保存できません！"
+	  render:index
+    end
+  end
 
-	def create
-		@message = Message.create(message_params)
-		if @message.save
-			flash[:success] = "保存されました！"
-	        redirect_to action: :index
-        else
-	        flash[:error] = "空欄！保存できません！"
-	        render:index
-    	end
-	end
-
-	private
-	def message_params
-		params.require(:message).permit(:body)
-	end
+  private
+  def message_params
+	params.require(:message).permit(:body)
+  end
 end
