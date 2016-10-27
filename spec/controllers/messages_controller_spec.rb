8e96fba0da280fbce5417983bfc57a1a2d29c7a1
kotasugimoto <sugimotokouta@sugimoto-no-MacBook-Pro.local>
2016-10-27 21:10:22 +0900

    コントローラーのテスト

---
+++ spec/controllers/messages_controller_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+describe MessagesController, type: :controller do
+	describe 'GET #index' do
+		it "messageのviewに遷移しているか" do
+			get :index
+			# expect(response).to render_template :index
+			expect(response).to have_http_status(:success)
+		end
+
+		it "インスタンス変数は期待した値になるか" do
+			# グループ未実装のため空白
+		end
+	end
+end
