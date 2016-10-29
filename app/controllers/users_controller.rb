e60234d940d861787dcfc77f8372533f8b2b52f6
kotasugimoto <sugimotokouta@sugimoto-no-MacBook-Pro.local>
2016-10-29 16:18:01 +0900

    ユーザー編集機能の追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,15 @@
 class UsersController < ApplicationController
   def edit
+    @user = User.find(params[:id])
+  end
+  def update
+    user = User.find(params[:id])
+    user.update_with_password(update_params)
+    redirect_to controller: 'messages',action: 'index'
+  end
+
+  private
+  def update_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :current_password)
   end
 end
