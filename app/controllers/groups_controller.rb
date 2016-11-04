76af7b9f19bd4e2f02291651f993d23ef8e6e108
kotasugimoto <sugimotokouta@sugimoto-no-MacBook-Pro.local>
2016-11-04 12:26:36 +0900

    保存処理

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -2,4 +2,20 @@ class GroupsController < ApplicationController
   def new
     @group = Group.new
   end
+  def create
+    @group = Group.create(group_params)
+  end
+  def edit
+
+  end
+  def index
+
+  end
+  def update
+
+  end
+  private
+  def group_params
+    params.require(:group).permit(:group_name, { user_ids:[] })
+  end
 end
