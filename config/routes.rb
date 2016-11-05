e78720e527cdce1e805faea34fc2a3ff60f3bdb7
kotasugimoto <sugimotokouta@sugimoto-no-MacBook-Pro.local>
2016-11-05 18:23:02 +0900

    親子関係の構築

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "messages#index"
+  root to: "groups#index"
 
-  resources :users, only: [:edit, :update]
-  resources :messages, only: [:index, :create]
-  resources :groups, only: [:new, :create, :edit, :update]
+  resources :users, only: [:edit, :update, :index]
+  resources :groups, only: [:new, :create, :edit, :update,:index] do
+    resources :messages, only: [:index, :create]
+  end
 end
