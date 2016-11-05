1d811c398c21d0e311802607c25a9c249b49393c
kotasugimoto <sugimotokouta@sugimoto-no-MacBook-Pro.local>
2016-11-05 18:24:51 +0900

    構造の変更と残り機能の実装
    まとまっていてすみません

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,26 +1,42 @@
 class GroupsController < ApplicationController
+  def index
+    @groups = Group.all
+    @message = Message.new
+  end
   def new
     @group = Group.new
   end
   def create
-    @group = Group.create(group_params)
+    @group = Group.create(create_params)
     if @group.save
-      redirect_to messages_path
+      redirect_to group_messages_path(@group), flash: {notice: 'グループが作成されました。'}
     else
       redirect_to action: 'new'
+      flash[:alert] = 'グループの作成に失敗しました'
     end
   end
   def edit
-
-  end
-  def index
-
+    @group = Group.find(params[:id])
+    @members = @group.members
   end
   def update
+    @group = Group.find(params[:id])
+    @members = @group.members
+    if @group.update(update_params)
+      redirect_to group_messages_path(@group), flash: {notice: '登録情報が変更されました。'}
+    else
+      redirect_to action: 'new'
+      flash[:alert] = 'グループの作成に失敗しました'
+    end
+  end
+  def show
 
   end
   private
-  def group_params
+  def update_params
+    params.require(:group).permit(:group_name, { user_ids: []})
+  end
+  def create_params
     params.require(:group).permit(:group_name, { user_ids:[] })
   end
 end
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,21 +1,24 @@
 class MessagesController < ApplicationController
   def index
+    @groups = Group.all
+    @group = Group.find(params[:group_id])
+    @messages = @group.messages
+    @members = @group.members
 	  @message = Message.new
   end
 
   def create
 	  @message = Message.new(message_params)
+    @group = Group.find(params[:group_id])
 	if @message.save
-	  flash[:success] = "保存されました！"
-	  redirect_to action: :index
+	  redirect_to group_messages_path(@group), flash: {alert: '保存されました'}
 	else
-	  flash[:error] = "空欄！保存できません！"
-	  render :index
+	  redirect_to group_messages_path(@group), flash: {alert: '本文を入力してください。'}
     end
   end
 
   private
   def message_params
-	  params.require(:message).permit(:body).merge(user_id: current_user.id)
+	  params.require(:message).permit(:body).merge(user_id: current_user.id, group_id: params[:group_id])
   end
 end
